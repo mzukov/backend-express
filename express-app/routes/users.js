95c156b1ae93aec9dcb3ad6fce2147cf5a47447c
mmdoneperson <mohmaks220@gmail.com>
2025-03-25 15:17:42 +0500

    add task 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  const users = {
+    items: [
+      {
+        id: 1,
+        name: "Иван Петров"
+      },
+      {
+        id: 2,
+        name: "Мария Сидорова"
+      },
+      {
+        id: 3,
+        name: "Алексей Иванов"
+      },
+      {
+        id: 4,
+        name: "Елена Смирнова"
+      },
+      {
+        id: 5,
+        name: "Дмитрий Кузнецов"
+      }
+    ]
+  };
+  res.json(users);
 });
 
 module.exports = router;
